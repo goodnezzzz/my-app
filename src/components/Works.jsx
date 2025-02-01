88ea8086353935f7f59d49fae7616a2a27d582f3
goodnezzzz <gerrardn46@gmail.com>
2025-02-01 13:09:09 +0100

    update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,19 +6,24 @@ import workImage2 from '../assets/work2.png'
 const Works = () => {
   return (
     <div className='workContainer'>
-        <div className='workImages'>
-            <div className='verticalImages'>
-                <div><img src={workImage} alt="" /></div>
-                <div><img src={workImage1} alt="" /></div>
-            </div>
-            <div className='centerImage'><img src={workImage2} alt="" /></div>
-        </div>
-        <div className='workTexts'>
-            <h1>How It Works</h1>
-            <p>Getajob helps you find the right job easily. Getajob helps you find the right job easily. Getajob helps you find the right job easily.</p>
-        </div>
+      <div className='verticalImages'>
+        <img src={workImage} alt="" />
+        <img src={workImage1} alt="" />
+      </div>
+      <div className='centerImage'><img src={workImage2} alt="" /></div>
+      <div className='workTexts'>
+      <h1>How It Works</h1>
+      <p>Getajob helps you find the right job easily. Getajob helps you find the right job easily. Getajob helps you find the right job easily.</p>
+      </div>
     </div>
   )
 }
+
+
+
+
+
+
+
 
 export default Works
