0f01d841aed9b17dfe596337c5c558585ac6e11a
goodnezzzz <gerrardn46@gmail.com>
2025-02-01 13:45:11 +0100

    update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -14,6 +14,12 @@ const Works = () => {
       <div className='workTexts'>
       <h1>How It Works</h1>
       <p>Getajob helps you find the right job easily. Getajob helps you find the right job easily. Getajob helps you find the right job easily.</p>
+      <h3>Search Jobs</h3>
+      <p>We have jobs in the most popular industries and with top companies. Plus, you can create alerts so you never miss a job opportunity.</p>
+      <h3>Create a Profile and Get Noticed</h3>
+      <p>Create a profile to showcase your skills and experience so recruiters can find you. Upload your CV so you can apply for jobs quickly and easily.</p>
+      <h3>Apply for jobs</h3>
+      <p>Find the training you need to improve your skills, or visit our Career Center for tips and advice to build your career.</p>
       </div>
     </div>
   )
